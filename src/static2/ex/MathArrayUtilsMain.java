8b3d27474a28796ba1a7c99a33b7b68ac6404c9d
MOONJJJJJ <yangmoonjj5@gmail.com>
2024-01-02 17:34:33 +0900

    7 - 문제와 풀이

--- src/static2/ex/MathArrayUtilsMain.java
+++ src/static2/ex/MathArrayUtilsMain.java
@@ -5,7 +5,7 @@ import static static2.ex.MathArrayUtils.*;
 public class MathArrayUtilsMain {
 
     public static void main(String[] args) {
-        int[] values = {1, 2, 3, 4, 5};
+        int[] values = {1, 2, 3, 4, 6};
         System.out.println("sum=" + sum(values));
         System.out.println("average=" + average(values));
         System.out.println("min=" + min(values));
